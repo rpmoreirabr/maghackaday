ff83a1b9f8b53ae4ace2cb031cb5b95f8e8efc50
Rodrigo Paixao Moreira da Costa <romoreira@mongeralaegon.com.br>
2019-11-05 15:06:26 -0300

    Tela de Perfil

--- scripts/index.js
+++ scripts/index.js
@@ -3,3 +3,58 @@ $(function() {
         window.location = "simulacao.html?Oferta=" + this.id
     });
 });
+
+function mascaraInteiro() {
+    if (event.keyCode < 48 || event.keyCode > 57) {
+        event.returnValue = false;
+        return false;
+    }
+    return true;
+}
+
+function MascaraCPF(cpf) {
+    if (mascaraInteiro(cpf) == false) {
+        event.returnValue = false;
+    }
+    return formataCampo(cpf, '000.000.000-00', event);
+}
+
+function MascaraData(data) {
+    if (mascaraInteiro(data) == false) {
+        event.returnValue = false;
+    }
+    return formataCampo(data, '00/00/0000', event);
+}
+
+//formata de forma generica os campos
+function formataCampo(campo, Mascara, evento) {
+    var boleanoMascara;
+
+    var Digitato = evento.keyCode;
+    exp = /\-|\.|\/|\(|\)| /g
+    campoSoNumeros = campo.value.toString().replace(exp, "");
+
+    var posicaoCampo = 0;
+    var NovoValorCampo = "";
+    var TamanhoMascara = campoSoNumeros.length;;
+
+    if (Digitato != 8) { // backspace 
+        for (i = 0; i <= TamanhoMascara; i++) {
+            boleanoMascara = ((Mascara.charAt(i) == "-") || (Mascara.charAt(i) == ".") ||
+                (Mascara.charAt(i) == "/"))
+            boleanoMascara = boleanoMascara || ((Mascara.charAt(i) == "(") ||
+                (Mascara.charAt(i) == ")") || (Mascara.charAt(i) == " "))
+            if (boleanoMascara) {
+                NovoValorCampo += Mascara.charAt(i);
+                TamanhoMascara++;
+            } else {
+                NovoValorCampo += campoSoNumeros.charAt(posicaoCampo);
+                posicaoCampo++;
+            }
+        }
+        campo.value = NovoValorCampo;
+        return true;
+    } else {
+        return true;
+    }
+}
